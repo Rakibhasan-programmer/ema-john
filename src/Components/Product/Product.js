187ffdf7f7a9ad51c09a5fc2c436c444efce3ae8
unknown <rakibulhasan.seu.cse@gmail.com>
2021-10-17 02:23:34 +0600

    shop and product show done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -15,6 +15,7 @@ const Product = (props) => {
                     <small>Price: {price}</small> <br />
                     <small>Only {stock} left in stock - order soon</small>
                 </p>
+                <button className='add-btn'>Add to Cart</button>
             </div>
         </div>
     );
